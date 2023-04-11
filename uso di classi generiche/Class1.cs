85909dfb285d7d328fa450370f8649c3bde1ee67
Piersilvio Spicoli <88087303+Piersilvio@users.noreply.github.com>
2023-04-11 14:20:00 +0200

    Add files via upload

---
+++ uso di classi generiche/Class1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Programm<T>
+    {
+        private List<T> l = new();
+
+        public List<T> getL() { return this.l; }
+        public void setL(List<T> l) { this.l = l; }
+
+        public static int sum(int x, int y) { return x + y; }
+
+        //overloading
+        public static double sum(double x, double y) { return x + y; }
+
+        public static int lunghezzaL(List<T> l)
+        {
+            int x = 0;
+
+            if (l.Count > 0)
+            {
+                x = l.Count;
+            }
+            else
+            {
+                x = 0;
+            }
+
+            return x; //restituisce la lunghezza di un generics
+        }
+    }
+}
---
+++ uso di classi generiche/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Diagnostics.CodeAnalysis;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Programm<double> program_double = new();
+            Programm<int> program_int = new();
+
+            Console.WriteLine($"senza overloading (somma intera): {Programm<int>.sum(3,4)}");
+            Console.WriteLine($"con overloading (somma in virgola mobile): {Programm<double>.sum(3.1, 4.4)}");
+            
+            
+            List<double> list = new List<double>();
+            List<int> list_int = new List<int>();
+
+            program_double.setL(list);
+            program_int.setL(list_int);
+
+            program_int.getL().Add(Programm<int>.sum(5, 10));
+            program_int.getL().Add(Programm<int>.sum(3, 10));
+
+            program_double.getL().Add(Programm<double>.sum(5.1, 10));
+            program_double.getL().Add(Programm<double>.sum(3, 10.3)); 
+            program_double.getL().Add(Programm<double>.sum(5.5, 10));
+            program_double.getL().Add(Programm<double>.sum(3.7, 10));
+
+            Console.WriteLine($"Lunghezza lista double: {Programm<double>.lunghezzaL(program_double.getL())}");
+            Console.WriteLine($"Lunghezza lista int: {Programm<int>.lunghezzaL(program_int.getL())}");
+        }
+    }
+}
