11cfd3c6f2c49b1ab0dfd0d50d33a1e9081a2daf
Piersilvio Spicoli <88087303+Piersilvio@users.noreply.github.com>
2023-04-13 10:35:54 +0200

    aggiunta esempio di utilizzo di stack e queue

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var pila = new Stack<int>();
+            var coda = new Queue<int>();
+
+            pila.Push(1);
+            pila.Push(2);
+            pila.Push(3);
+
+            coda.Enqueue(pila.Count);   //inserisco il #(el) della pila
+            coda.Enqueue(pila.Pop());   //inserisco in coda l'ultimo elemento poppato
+            coda.Enqueue(coda.Count);   //inserisco in coda il #(el) della pila
+
+            Console.WriteLine("Pila:");
+            foreach (int i in pila)
+            {
+                Console.WriteLine($"\t{i}");
+            }
+
+            Console.WriteLine("\nCoda:");
+            foreach (int i in coda)
+            {
+                Console.WriteLine($"\t{i}");
+            }
+        }
+    }
+}
