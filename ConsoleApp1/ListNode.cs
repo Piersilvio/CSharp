3cce042c7bb1b26c3704381e8528da86f07528bc
Piersilvio Spicoli <88087303+Piersilvio@users.noreply.github.com>
2023-04-14 11:27:20 +0200

    Add files via upload

---
+++ ConsoleApp1/ListNode.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+//esempio di classe parametrica
+public class ListNode<T>
+{
+
+	public T Value { get; set; }
+	public ListNode<T> NextNode { get; set; }
+	
+}
+
+public class Utils
+{
+    //metodo generico in una classe non genrica
+    public static void Swap<T>(ref T first, ref T second)
+    {
+        T temp = first;
+        first = second;
+        second = temp;
+    }
+
+    public static T Max<T>(IEnumerable<T> list) where T : IComparable //vincolo
+    {
+        bool isFirst = true;
+        T? res = default(T);
+
+        foreach (T item in list)
+        {
+            if (isFirst)
+            {
+                res = item;
+                isFirst = false;
+            }
+            if (res.CompareTo(item) >= 0)
+                continue;
+            res = item;
+        }
+
+        return res;
+    }
+
+    public static void testNullable()
+    {
+        int myInt = default(int);
+        Nullable<int> nullableInt = default(Nullable<int>);
+
+        Console.WriteLine(myInt); // Stampa 0
+        Console.WriteLine(nullableInt); // Stampa una riga vuota
+
+        myInt = 5;
+        nullableInt = 7; // Assegnazione di un valore intero
+        var res = myInt + nullableInt; // res è di tipo Nullable<int>
+
+        Console.WriteLine(res);
+
+        if (nullableInt.HasValue) // Verifica presenza di un valore
+        {
+            var value = nullableInt.Value; // Contiene il valore int
+            Console.WriteLine(value);
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var utils = new Utils();
+            int a = 4, b = 5;
+
+            int[] v = new int[] { 19, 23, 0, 4, 7 };
+            List<int> l = new List<int>(v);
+
+
+            Utils.Swap(ref a, ref b);
+            
+            Console.WriteLine($"a = {a}, b = {b}");
+            Console.WriteLine($"max l = {Utils.Max(l)}\n");
+
+            Utils.testNullable();
+        }
+    }
+}
