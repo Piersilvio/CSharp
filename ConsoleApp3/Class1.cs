37f3719eea5bcbd82fab0cdc3805008cd44dbcf9
Piersilvio Spicoli <88087303+Piersilvio@users.noreply.github.com>
2023-04-20 11:44:52 +0200

    uso dei delegate 2

---
+++ ConsoleApp3/Class1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    public class Logger
+    {
+        public delegate void StringLogWriter(DateTime timestamp, string message);
+
+        private StringLogWriter writer;
+
+        public Logger(StringLogWriter writer)
+        {
+            this.writer = writer;
+        }
+ 
+        public void Log(string message)
+        {
+            if (this.writer != null)
+                this.writer(DateTime.Now, message);
+        }
+    }
+}
---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,42 @@
+﻿using static ConsoleApp3.Logger;
+
+namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Definizione del primo delegate
+            var writer = new FileLogWriter(@"c:\somefile.txt");
+            var fileDelegate = new StringLogWriter(writer.Write);
+
+            // Definizione del secondo delegate
+            var consoleDelegate = new StringLogWriter(ConsoleWriter);
+
+            // Combinazione dei delegate
+            StringLogWriter combinedDelegate = (StringLogWriter)Delegate.Combine(consoleDelegate, fileDelegate);
+            var myLogger = new Logger(combinedDelegate);
+
+            // Scrive sulla console e su file il messaggio in basso
+            myLogger.Log("Messaggio di esempio");
+        }
+
+        private static void ConsoleWriter(DateTime timestamp, string message)
+        {
+            Console.WriteLine("{0} - {1}", timestamp, message);
+        }
+    }
+
+    public class FileLogWriter
+    {
+        public string FileName { get; set; }
+        public FileLogWriter(string filename)
+        {
+            this.FileName = filename;
+        }
+        public void Write(DateTime timestamp, string message)
+        {
+            // Scrittura messaggio su file...
+        }
+    }
+}
