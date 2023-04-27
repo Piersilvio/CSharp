4d8cb581002de39292d8bd408c5a17a8f2a014b2
Piersilvio Spicoli <88087303+Piersilvio@users.noreply.github.com>
2023-04-27 12:46:42 +0200

    esempio di IDisposable

---
+++ ConsoleApp2/DisposableObject.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    /*
+     * implemento i metodi presenti in IDisposable!
+     */
+    internal class DisposableObject : IDisposable
+    {
+        //reso virtual in modo da poter effettuare poi @Ovveride
+        protected virtual void DisposeObject(bool disposing)
+        {
+            if (disposing)
+            {
+                //si liberano le risorse gestite
+            }
+
+            //si liberano le risorse non gestite
+        }
+        
+        //overloading della firma del metodo dell'IDisposable
+        public void Dispose()
+        {
+            this.DisposeObject(true);
+            GC.SuppressFinalize(this);
+        }
+
+        //distruttore che forza la liberazione delle risorse non gestite
+        ~DisposableObject() { this.DisposeObject(false); }
+
+        public string someMethod(string str) { return str; }
+    }
+}
--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -4,27 +4,25 @@
     {
         static void Main(string[] args)
         {
-            var pila = new Stack<int>();
-            var coda = new Queue<int>();
+            /*
+             * invoco la classe per gestire
+             * la deallocazione dell'oggetto
+             */
+            var mioOggetto = new DisposableObject();
 
-            pila.Push(1);
-            pila.Push(2);
-            pila.Push(3);
-
-            coda.Enqueue(pila.Count);   //inserisco il #(el) della pila
-            coda.Enqueue(pila.Pop());   //inserisco in coda l'ultimo elemento poppato
-            coda.Enqueue(coda.Count);   //inserisco in coda il #(el) della pila
-
-            Console.WriteLine("Pila:");
-            foreach (int i in pila)
+            try
             {
-                Console.WriteLine($"\t{i}");
-            }
-
-            Console.WriteLine("\nCoda:");
-            foreach (int i in coda)
+                Console.WriteLine($"{mioOggetto.someMethod("Pierino")}");  
+            }catch(Exception ex)
             {
-                Console.WriteLine($"\t{i}");
+                Console.WriteLine(ex.ToString());
+            }finally { 
+                /*
+                 * dopo che il try{} termina e non 
+                 * si sono verificate eccezioni, allora l'oggetto
+                 * viene deallocato! (non intervine il garbage!!)
+                 */
+                mioOggetto.Dispose();
             }
         }
     }
