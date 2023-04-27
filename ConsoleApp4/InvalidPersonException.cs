d7a9f2a75ba884cacfd9c0c701ed25260d7b64c6
Piersilvio Spicoli <88087303+Piersilvio@users.noreply.github.com>
2023-04-27 10:52:53 +0200

    eccezione customizzata

---
+++ ConsoleApp4/InvalidPersonException.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    internal class Person
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        
+        public Person(int id, string name) { 
+            this.Id = id;
+            this.Name = name;
+        }
+
+        public int incrementoId() { 
+            
+            if (this.Id < 0)
+            {
+                throw new InvalidPersonException("è sbagliato"); //lancio l'eccezione personalizzata
+            }
+            
+            return this.Id; 
+        }
+    }
+
+    [Serializable]
+    public class InvalidPersonException: Exception
+    {
+        /*
+         * eredito il costruttore
+         * di exception
+         */
+        public InvalidPersonException(string message):base(message) {}  
+
+       public void PrintMessage(InvalidPersonException e)
+        {
+            Console.WriteLine(e.Message);
+        }
+    }
+}
--- ConsoleApp4/Program.cs
+++ ConsoleApp4/Program.cs
@@ -1,29 +1,18 @@
-﻿using System.Drawing;
-
-namespace ConsoleApp4
+﻿namespace ConsoleApp4
 {
     internal class Program
     {
-        // declaring delegate
-        public delegate string MyDelegate<T>(T input) where T : struct;
-
-        private static string DateTimeWriter(DateTime input, string n)
-        {
-            return input.ToShortDateString();
-        }
-
-        private static string Something(DateTime input, string n)
-        {
-            return "ciao";
-        }
-
         static void Main(string[] args)
         {
-            var p = new Program();
-            var sample = new Func<DateTime, string, string>(DateTimeWriter);
+            var p = new Person(-1, "piero"); //il -1 genererà l'eccezione
 
-            sample += Program.Something;
-            Console.WriteLine($"{sample}");
+            try
+            {
+                p.incrementoId();
+            }catch (InvalidPersonException e) //catturo un eccezione personalizzata
+            {
+                e.PrintMessage(e);
+            }
         }
     }
 }
