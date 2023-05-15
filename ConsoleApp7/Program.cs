32bc4e8762319edf73a25100f7bcc966cace58c3
Piersilvio Spicoli <88087303+Piersilvio@users.noreply.github.com>
2023-05-15 11:22:26 +0200

    primo esempio con LINQ

---
+++ ConsoleApp7/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace ConsoleApp7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * un array è possibile enumerarlo
+             * quini IEnumerable<T> funziona e 
+             * funziona anche LINQ!
+             */
+
+            int[] vector = { 1, 2, 3, 0, 54, 4, 102, 65};
+
+            /*
+             * "per ogni x in vector
+             * dove x > 50 ordina x in decrescente e estrapola x"
+             * 
+             * a differenza di SQL, l'ordine di LINQ è inverso!!!!
+             */
+            IEnumerable<int> query =
+                from x in vector where x > 50 orderby x descending select x;
+
+            //mostro il risultato della query
+            foreach (int x in query)
+            {
+                Console.WriteLine(x);
+            }
+
+            //----------- con l'uso di un generics (enumerabile) ------------------
+
+            //istanzio una lista di interi
+            List<int> list = new List<int>(vector);
+
+            var q = from x in list where x > 50 orderby x descending select x;
+
+            foreach (int x in q)
+            {
+                Console.WriteLine(x);
+            }
+        }
+    }
+}
