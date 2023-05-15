0f9bb8fc0953658fd6ebf0b4dd13dae64fac0ac0
Piersilvio Spicoli <88087303+Piersilvio@users.noreply.github.com>
2023-05-15 11:20:28 +0200

    uso della classe Task<T>

--- ConsoleApp4/Program.cs
+++ ConsoleApp4/Program.cs
@@ -1,18 +1,37 @@
-﻿namespace ConsoleApp4
+﻿using System.Runtime.CompilerServices;
+
+namespace ConsoleApp4
 {
     internal class Program
     {
+       public static int incremento(int a) {
+
+            return a += 1; 
+        }
+
         static void Main(string[] args)
         {
-            var p = new Person(-1, "piero"); //il -1 genererà l'eccezione
+            //creazione di un task semplice
+            var simpleTask = Task.Factory.StartNew(() =>
+            {
+                Thread.Sleep(1000);
+                Console.WriteLine("ciao da SimpleTask");
+            });
 
-            try
+            //costruzione di un task con parametro in input
+            var parameterTask = Task.Factory.StartNew((nome) =>
             {
-                p.incrementoId();
-            }catch (InvalidPersonException e) //catturo un eccezione personalizzata
-            {
-                e.PrintMessage(e);
-            }
-        }
+                Thread.Sleep(1000);
+                Console.WriteLine("ciao da {0}", nome);
+            }, "pieroSilvio");
+
+            var resTask = Task.Factory.StartNew((valInput) => incremento(4), 4);
+
+            //metto in attesa il thread chiamante per far eseguire il thread
+            resTask.Start();
+            resTask.Wait();
+            simpleTask.Wait();
+            parameterTask.Wait();
+        }   
     }
 }
