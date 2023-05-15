d83a454115e94b7056ab7a12ce64eca2f1addc43
Piersilvio Spicoli <88087303+Piersilvio@users.noreply.github.com>
2023-05-15 11:21:03 +0200

    altro esempio con la classe Task<T>

---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace ConsoleApp5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var compositeTask = Task.Factory.StartNew(() =>
+            {
+                Thread.Sleep(1000);
+                Console.WriteLine("Primo task");
+            }).ContinueWith((t) =>
+            {
+                Thread.Sleep(1000);
+                Console.WriteLine("Secondo task");
+            });
+
+            // Accodamento di una funzione
+            var resultTask = compositeTask.ContinueWith((t) =>
+            {
+                string result = "Funzione del terzo task";
+                Console.WriteLine(result);
+                return result;
+            });
+
+            Console.WriteLine("Il risultato è: {0}", resultTask.Result);
+        }
+    }
+}
