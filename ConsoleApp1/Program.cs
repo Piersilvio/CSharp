eba85823dbd4502d15e9877dfdb4d955cd629865
Piersilvio Spicoli <88087303+Piersilvio@users.noreply.github.com>
2023-05-15 11:15:53 +0200

    esempio di un thread banale

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -4,19 +4,17 @@
     {
         static void Main(string[] args)
         {
-            var utils = new Utils();
-            int a = 4, b = 5;
+           
+            Thread t2 = new Thread(() => {
+                Thread.Sleep(8000); //8 sec.
+                Console.WriteLine("Partenza thread t2");
+            });
 
-            int[] v = new int[] { 19, 23, 0, 4, 7 };
-            List<int> l = new List<int>(v);
+            //esecuzione t2
+            t2.Start();
 
-
-            Utils.Swap(ref a, ref b);
-            
-            Console.WriteLine($"a = {a}, b = {b}");
-            Console.WriteLine($"max l = {Utils.Max(l)}\n");
-
-            Utils.testNullable();
+            //esecuzione thread principale
+            Console.WriteLine("partenza thread principale main");
         }
     }
 }
